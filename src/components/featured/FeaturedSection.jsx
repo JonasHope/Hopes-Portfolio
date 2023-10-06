e2e4762af16e87b0ab49cf43753335b545851263
JonasHope <95291501+JonasHope@users.noreply.github.com>
2023-10-06 10:44:02 +0200

    no overlay?

--- src/components/featured/FeaturedSection.jsx
+++ src/components/featured/FeaturedSection.jsx
@@ -20,14 +20,14 @@ const BackgroundVideo = styled.video`
   pointer-events: none;
 `;
 
-const Overlay = styled.div`
+/*const Overlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
-`;
+`;*/
 
 function VideoPlayer() {
   return (
@@ -35,7 +35,7 @@ function VideoPlayer() {
       <BackgroundVideo autoPlay muted loop>
         <source src={featuredVideo} type="video/mp4"></source>
       </BackgroundVideo>
-      <Overlay />
+
       <TextTypingAnimation />
     </BackgroundVideoContainer>
   );
